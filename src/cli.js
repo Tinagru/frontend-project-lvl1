2bcf80a7ef1cfd2ad8a393e1764bd2a286f79f33
Грушихина <Gvalya93@mail.ru>
2022-04-26 22:58:58 +0300

    add cli-even.js, brain-even.js

--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,7 @@
 import readlineSync from 'readline-sync';
 
-console.log('Welcome to the Brain Games!');
-export const userName = readlineSync.question("May I have your name?");
-console.log(`Hello, ${userName}!`);
+const greeting = () => {
+  const userName = readlineSync.question('May I have your name?');
+  return console.log(`Hello, ${userName}!`);
+};
+export default greeting;
