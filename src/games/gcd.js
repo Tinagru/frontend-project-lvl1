95ddab6340ad423d7c1bde4050ba344b30586913
Грушихина <Gvalya93@mail.ru>
2022-05-08 23:44:20 +0300

    code optimization

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -6,15 +6,17 @@ const task = 'Find the greatest common divisor of given numbers.';
 
 const min = 0;
 const max = 100;
+
 const gcd = (num1, num2) => {
   if (!num2) {
     return num1;
   }
   return gcd(num2, num1 % num2);
 };
-const num1 = getRandomValue(min, max);
-const num2 = getRandomValue(min, max);
+
 const getData = () => {
+  const num1 = getRandomValue(min, max);
+  const num2 = getRandomValue(min, max);
   const answer = String(gcd(num1, num2));
   const question = `${num1} ${num2}`;
   return { question, answer };
