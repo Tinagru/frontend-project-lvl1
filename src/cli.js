04cb00eca3389d1cf958c3bf67d0049ffd19b5b1
Грушихина <Gvalya93@mail.ru>
2022-05-08 17:10:40 +0300

    remove extra files from src

--- src/cli.js
+++
@@ -1,7 +0,0 @@
-import readlineSync from 'readline-sync';
-
-const greeting = () => {
-  const userName = readlineSync.question('May I have your name?');
-  return console.log(`Hello, ${userName}!`);
-};
-export default greeting;
